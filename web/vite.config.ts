cda1cc2a480831a40e837d2ffe0efc2d8f58d087
TheRemyyy <theremyyy@gmail.com>
2025-12-28 20:44:26 +0100

    Optimize web build: Gzip, Terser minification, manualChunks, and SEO meta tags

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [
+        react(),
+        compression({
+            algorithm: 'gzip',
+            ext: '.gz',
+        })
+    ],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                    utils: ['marked'],
+                },
+            },
+        },
+    }
 })
